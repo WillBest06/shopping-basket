08b2b1158eeac9fe8c22bf1deb8dd0afe884a6b6
William Best <148398824+WillBest06@users.noreply.github.com>
2025-10-13 15:33:39 +0100

    simplify jsx and add basket quantity indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,63 @@
 import { NavLink, Outlet } from "react-router";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [basketItems, setBasketItems] = useState([]);
+
+  const getBasketQuantity = () => {
+    return basketItems.length > 0
+      ? basketItems.reduce(
+          (numOfItems, currentItem) => numOfItems + currentItem.quantity,
+          0
+        )
+      : 0;
+  };
+
   return (
     <>
       <nav className={styles.site_header_nav}>
         <ul className={styles.nav_menu}>
-          <li className={styles.nav_item}>
-            <NavLink
-              to={"home"}
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles.nav_link} ${styles.active}`
-                  : `${styles.nav_link}`
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className={styles.nav_item}>
-            <NavLink
-              to={"shop"}
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles.nav_link} ${styles.active}`
-                  : `${styles.nav_link}`
-              }
-            >
-              Shop
-            </NavLink>
-          </li>
-          <li className={styles.nav_item}>
-            <NavLink
-              to={"basket"}
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles.nav_link} ${styles.active}`
-                  : `${styles.nav_link}`
-              }
-            >
-              Basket
-            </NavLink>
-          </li>
+          {["Home", "Shop", "Basket"].map((page) => {
+            const pageLower = page.toLowerCase();
+
+            if (pageLower === "basket")
+              return (
+                <li key={pageLower} className={styles.nav_item}>
+                  <NavLink
+                    to={pageLower}
+                    className={({ isActive }) =>
+                      isActive
+                        ? `${styles.nav_link} ${styles.active}`
+                        : `${styles.nav_link}`
+                    }
+                  >
+                    {page}
+                  </NavLink>
+                  <span className={styles.basketSize}>
+                    {getBasketQuantity()}
+                  </span>
+                </li>
+              );
+
+            return (
+              <li key={pageLower} className={styles.nav_item}>
+                <NavLink
+                  to={page.toLowerCase()}
+                  className={({ isActive }) =>
+                    isActive
+                      ? `${styles.nav_link} ${styles.active}`
+                      : `${styles.nav_link}`
+                  }
+                >
+                  {page}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
       </nav>
-      <Outlet />
+      <Outlet context={[basketItems, setBasketItems]} />
     </>
   );
 }
